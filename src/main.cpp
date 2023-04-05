2f40d7e5abe8aa7ccbcd784897cb86fe84a55312
Nirajan Dhakal <nirajan.data@gmail.com>
2023-04-05 14:01:04 -0600

    add:delete confess

--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,22 @@ int main() {
     env>>BOT_TOKEN;
     env.close();
  */
-    const std::string BOT_TOKEN=TOKEN_ID;
-    const int64_t CID=1093248426076020787;
+    const std::string BOT_TOKEN=ENV_TOKEN;
+//    const int64_t CID=ENV_CID;
 
     dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
 
     bot.on_log(dpp::utility::cout_logger());
 
     bot.on_message_create([&bot](const dpp::message_create_t & event) {
-        if (event.msg.channel_id== CID && event.msg.content.size()>0) {
+        if (event.msg.channel_id== ENV_CID && event.msg.content.size()>0) {
             dpp::embed embed = dpp::embed().
                     set_color(dpp::colors::sti_blue).
-                    set_title("confession").
+                    set_title("Confession").
                     set_description(event.msg.content).
                     set_timestamp(time(0));
 
-            /* reply with the created embed */
+            bot.message_delete(event.msg.id,event.msg.channel_id);
             bot.message_create(dpp::message(event.msg.channel_id,embed));
         }
     });
